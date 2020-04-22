2b8d1280387b0d1d0cdea1315eb5d511db66c9be
Akash <gsaisriram.akash@gmail.com>
2020-04-22 20:26:12 +0530

    improved box shadows

---
+++ script.js
@@ -0,0 +1,47 @@
+
+window.sr = ScrollReveal();
+
+
+sr.reveal('.nav-container', {
+  duration:3500,
+  origin:'bottom'
+});
+
+
+sr.reveal('.home-text', {
+    duration:3000,
+    origin:'left',
+    distance:'300px'
+});
+
+sr.reveal('.home-image', {
+    duration:3000,
+    origin:'right',
+    distance:'300px'
+});
+
+sr.reveal('.about-image', {
+    duration:3000,
+    origin:'left',
+    distance:'300px'
+});
+
+sr.reveal('.about-text', {
+    duration:3000,
+    origin:'right',
+    distance:'300px'
+});
+
+
+sr.reveal('.precautions-container', {
+    duration:4000,
+    origin:'left',
+    distance:'300px'
+});
+
+sr.reveal('.virus-img', {
+    duration:4000,
+    origin:'right',
+    distance:'300px'
+});
+
